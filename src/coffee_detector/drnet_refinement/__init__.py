f16d974e4db08cdf2a2c14596e84cea320d1d1c4
ediprin <ediprin.audri1997@gmail.com>
2026-08-07 20:25:58 +0700

    export DRNet interaction verification components

--- src/coffee_detector/drnet_refinement/__init__.py
+++ src/coffee_detector/drnet_refinement/__init__.py
@@ -1,3 +1,14 @@
+from .interaction import (
+    DRNetCoarseBranch,
+    DRNetInteractionConfig,
+    DRNetInteractionDetectionModel,
+    DRNetInteractionDetectHead,
+    build_entity_family_mapping,
+    load_drnet_interaction_weights,
+    verify_fine_logits,
+)
+from .interaction_loss import DRNetInteractionDetectionLoss
+from .interaction_trainer import make_drnet_interaction_trainer
 from .loss import DRNetDetectionLoss, confusion_minimized_positive_loss
 from .model import (
     DRNetFineGrainedBranch,
@@ -19,4 +30,13 @@ __all__ = [
     "confusion_minimized_positive_loss",
     "load_drnet_refinement_weights",
     "make_drnet_refinement_trainer",
+    "DRNetInteractionConfig",
+    "DRNetCoarseBranch",
+    "DRNetInteractionDetectHead",
+    "DRNetInteractionDetectionModel",
+    "DRNetInteractionDetectionLoss",
+    "build_entity_family_mapping",
+    "verify_fine_logits",
+    "load_drnet_interaction_weights",
+    "make_drnet_interaction_trainer",
 ]
