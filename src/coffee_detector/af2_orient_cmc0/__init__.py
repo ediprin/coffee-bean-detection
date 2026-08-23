8706dae3622ac9f9f688e5b24046c03fc874ac1c
ediprin <ediprin.audri1997@gmail.com>
2026-08-23 17:56:46 +0700

    export AF2_ORIENT CMC0 static audit

--- src/coffee_detector/af2_orient_cmc0/__init__.py
+++ src/coffee_detector/af2_orient_cmc0/__init__.py
@@ -1,3 +1,4 @@
+from .audit import static_af2_orient_cmc0_audit
 from .model import AF2OrientCMC0DetectionModel, load_af2_orient_cmc0_weights
 from .trainer import make_af2_orient_cmc0_trainer
 
@@ -5,4 +6,5 @@ __all__ = [
     "AF2OrientCMC0DetectionModel",
     "load_af2_orient_cmc0_weights",
     "make_af2_orient_cmc0_trainer",
+    "static_af2_orient_cmc0_audit",
 ]
