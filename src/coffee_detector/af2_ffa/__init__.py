b89e34b30903e7dedb64a8ac714eb86fad7e3c3f
ediprin <ediprin.audri1997@gmail.com>
2026-08-23 22:44:47 +0700

    Export from-start audit and DCT descriptor

--- src/coffee_detector/af2_ffa/__init__.py
+++ src/coffee_detector/af2_ffa/__init__.py
@@ -9,6 +9,8 @@ from .model import (
 )
 from .trainer import make_af2_ffa_trainer
 from .audit import run_af2_ffa_static_audit
+from .from_start_audit import run_af2_ffa_from_start_static_audit
+from .dct import DCT_HIGH_FREQUENCY_PAIRS, selected_dct_descriptor
 
 __all__ = [
     "AF2FFAConfig",
@@ -18,4 +20,7 @@ __all__ = [
     "load_af2_ffa_weights",
     "make_af2_ffa_trainer",
     "run_af2_ffa_static_audit",
+    "run_af2_ffa_from_start_static_audit",
+    "DCT_HIGH_FREQUENCY_PAIRS",
+    "selected_dct_descriptor",
 ]
